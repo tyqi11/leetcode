b43fbc295999e22ba0a59a8b0bfa5039a1ff4696
tyqi11 <443564687@qq.com>
2019-07-06 19:17:42 +0800

    String

---
+++ 3_Longest_Substring_Without_Repeating_Characters.java
@@ -0,0 +1,39 @@
+/*
+
+1. To get a substring, we need to know the start index and end index. To get the longest,
+we need an extra max variable. When iterating over all the characters in the string, if
+the current character appeared at index i before, the substring can only start from i + 1.
+And from i + 1 to current index, we get the length of the substring and compare with 
+the max. 
+
+2. Next time we find another repeating character, the start index is updated to, not i + 1,
+but Math.max(start, i + 1) because we still need to follow the rules set by previous
+characters.
+
+*/
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        if (s.length() < 2) {
+            return s.length();
+        }
+        int[] count = new int[256];
+        Arrays.fill(count, -1);
+        int max = 0;
+        int start = 0; // [start, i] is the result length
+        for (int i = 0; i < s.length(); i++) {
+            int idx = s.charAt(i);
+            if (count[idx] != -1) {
+                start = Math.max(start, count[idx] + 1);
+            }
+            count[idx] = i;
+            max = Math.max(max, i - start + 1);
+        }
+        return max;
+    }
+}
+
+/*
+Time complexity: O(n)
+Space complexity: O(1)
+*/
