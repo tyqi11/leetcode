1a586f5c0fd8e46060b55735b77d42e26a0638b1
tyqi11 <443564687@qq.com>
2019-11-03 21:44:43 -0800

    Facebook 62/233

---
+++ 211_Add_and_Search_Words-Data_structure_design.java
@@ -0,0 +1,54 @@
+class WordDictionary {
+    public class TrieNode {
+        public TrieNode[] children = new TrieNode[26];
+        public boolean isWord;
+    }
+    
+    private TrieNode root;
+    
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new TrieNode();
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+        TrieNode node = root;
+        for (char c : word.toCharArray()) {
+            if (node.children[c - 'a'] == null) {
+                node.children[c - 'a'] = new TrieNode();
+            }
+            node = node.children[c - 'a'];
+        }
+        node.isWord = true;
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        return match(word, 0, root);    
+    }
+    
+    private boolean match(String word, int idx, TrieNode node) {
+        if (idx == word.length()) {
+            return node.isWord;
+        }
+        char c = word.charAt(idx);
+        if (c != '.') {
+            return node.children[c - 'a'] != null && match(word, idx + 1, node.children[c - 'a']);
+        } else {
+            for (int i = 0; i < 26; i++) {
+                if (node.children[i] != null && match(word, idx + 1, node.children[i])) {
+                    return true;
+                }
+            }
+            return false;
+        }      
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
--- 340_159_Longest_Substring_with_At_Most_K_Distinct_Characters.java
+++ 340_159_Longest_Substring_with_At_Most_K_Distinct_Characters.java
@@ -1,1 +1,24 @@
-// Prime
+class Solution {
+    public int lengthOfLongestSubstringKDistinct(String s, int k) {
+        int[] count = new int[256];
+        int distinct = 0;
+        int ans = 0;
+        int i = 0;
+        for (int j = 0; j < s.length(); j++) {
+            char c = s.charAt(j);
+            if (count[c] == 0) {
+                distinct++;
+            }
+            count[c]++;
+            while (distinct > k && i <= j) {
+                count[s.charAt(i)]--;
+                if (count[s.charAt(i)] == 0) {
+                    distinct--;
+                }
+                i++;
+            }
+            ans = Math.max(ans, j - i + 1);
+        }
+        return ans;
+    }
+}
